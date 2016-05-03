70f59df57a3e1e0395581468335185809ad09fe1
ljqhack <ljqhack@qq.com>
2016-05-03 23:13:00 +0800

    0503

--- json.c
+++ json.c
@@ -1,1 +1,25 @@
- 
+#include "json.h"
+
+static JSON_t * NewItem(void)
+{
+    JSON_t *node = (JSON_t*)malloc(sizeof(JSON_t));
+    if(node)
+    {
+        memset(node, 0, sizeof(JSON_t));
+    }
+    return node;
+}
+
+void DeleteItem(JSON_t *c)
+{
+    JSON_t *node = c;
+    while(node)
+    {
+        
+    }
+}
+
+JSON_t *Parse(const char *value)
+{
+    
+}
--- json.h
+++ json.h
@@ -1,1 +1,28 @@
- 
+#ifndef __JSON_H__
+#define __JSON_H__
+
+/*JSON type define*/
+#define JSON_False      (1<<0)
+#define JSON_True       (1<<1)
+#define JSON_NULL       (1<<2)
+#define JSON_Number     (1<<3)
+#define JSON_String     (1<<4)
+#define JSON_Array      (1<<5)
+#define JSON_Object     (1<<6)
+
+
+typedef struct JSON_t{
+    struct JSON_t *next,*prev;
+    struct JSON_T *child;
+    int type;
+    char *valuestring;
+    int valueint;
+    double valuedouble;
+    char *string;    
+} JSON_t;
+
+
+extern JSON_t *Parse(const char *value);
+
+
+#endif 
